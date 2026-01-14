3af6f8ff762a7e095c73fe41aa3bfd73eafac170
Rishi Patel <rishi_1607@icloud.com>
2026-01-13 19:05:09 -0600

    Utilize Buttons instead of typing for options.

--- rockit.js
+++ rockit.js
@@ -1,14 +1,13 @@
 
 
+
+
+
+
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random() * 3);
     return choices[index];
-}
-
-function getHumanChoice() {
-    let input = prompt("Rock, Paper, or Scissors?");
-    return input;
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -23,6 +22,7 @@ function playRound(humanChoice, computerChoice) {
             humanChoice === "paper" && computerChoice === "rock" ||
             humanChoice === "scissors" && computerChoice === "paper")){
                 humanScore++;
+
                 return `You win! ${humanChoice} beats ${computerChoice}.`;
             }
         else {
@@ -35,12 +35,59 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame(){
+    const container = document.querySelector("#container");
+
+    const scoreCont = document.querySelector("#scoreRow");
+
+    let hScore = document.querySelector("#hScore");
+
+    let cScore = document.querySelector("#cScore");
+
+    let rock = document.createElement("button");
+    rock.setAttribute("id", "rock");
+
+    let paper = document.createElement("button");
+    paper.setAttribute("id", "paper");
+
+    let scissor = document.createElement("button");
+    scissor.setAttribute('id', 'scissor');
+
+        
+
+    container.appendChild(rock);
+    container.appendChild(paper);
+    container.appendChild(scissor);
+
+    let playerSelection;
+
     let humanScore = 0;
+    hScore.textContent = `Your Score: ${humanScore}`;
+
     let computerScore = 0;
+    cScore.textContent = `Computer Score: ${computerScore}`;
+    
+    container.addEventListener('click', (event) => {
+
+        let target = event.target;
+
+            switch(target.id) {
+                case 'rock':
+                    playerSelection = "Rock";
+                    break;
+
+                case 'paper':
+                    playerSelection = "Paper";
+                    break;
+
+                case 'scissor':
+                    playerSelection = "Scissor";
+                    break;
+
+            }
+            
+        })
 
     function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-        
 
         if (humanChoice === computerChoice) {
             return "It's a tie! Nobody wins this round.";
@@ -50,19 +97,26 @@ function playGame(){
                 humanChoice === "Paper" && computerChoice === "Rock" ||
                 humanChoice === "Scissors" && computerChoice === "Paper")){
                     humanScore++;
+                    hScore.textContent = `Your Score: ${humanScore}`;
                     return `You win! ${humanChoice} beats ${computerChoice}.`;
                 }
             else {
                 computerScore++;
+                cScore.textContent = `Computer Score: ${computerScore}`;
                 return `You lose! ${computerChoice} beats ${humanChoice}`;
             }
             
         }
     }
-
-    for (let i = 1; i <= 5; i++){
-        console.log(playRound(getHumanChoice(), getComputerChoice()));
+    
+    
+    for (let i = 0; i <= 5; i++) {
+        playRound(playerSelection, getComputerChoice());
     }
+
+
+    
+
 }
 
 playGame();
